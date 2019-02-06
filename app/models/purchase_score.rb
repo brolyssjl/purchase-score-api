6805099e0d121d970fd4c68ed6fcf9e0436ecdbb
Jonatan Bonilla <pescadin138@gmail.com>
2019-02-05 23:54:08 -0500

    add rspec validations to purchase_shopping model attributes

--- app/models/purchase_score.rb
+++ app/models/purchase_score.rb
@@ -1,2 +1,5 @@
 class PurchaseScore < ApplicationRecord
+  validates :score, :description, :user_id, :shop_id, :purchase_id, presence: true
+  validates :purchase_id, uniqueness: true
+  validates :score, numericality: { only_integer: true, less_than_or_equal_to: 5 }
 end
--- spec/models/purchase_score_spec.rb
+++ spec/models/purchase_score_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseScore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  it { should validate_presence_of(:score) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:shop_id) }
+  it { should validate_presence_of(:purchase_id) }
+  # Validate uniqueness of purchase_id
+  it { should validate_uniqueness_of(:purchase_id) }
+  it { should validate_numericality_of(:score).only_integer }
+  it { should validate_numericality_of(:score).is_less_than_or_equal_to(5) }
 end
