591df46fd15d057a7b9d9d7d5c78aed7494641d5
Jonatan Bonilla <pescadin138@gmail.com>
2019-02-06 00:22:50 -0500

    add seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  PurchaseScore.create(
+    score: Faker::Number.between(1, 5),
+    description: Faker::Lorem.paragraph,
+    deleted: Faker::Boolean.boolean(0.5),
+    user_id: Faker::Number.between(1, 10),
+    shop_id: Faker::Number.between(1, 10),
+    purchase_id: Faker::Number.between(1, 10)
+  )
+end
