8bd2b3941ebcd23734c65593cc083d5e22fda103
Jonatan Bonilla <pescadin138@gmail.com>
2019-02-06 23:56:46 -0500

    add scope to purchase_score model to find by parameters

--- app/models/purchase_score.rb
+++ app/models/purchase_score.rb
@@ -2,4 +2,6 @@ class PurchaseScore < ApplicationRecord
   validates :score, :description, :user_id, :shop_id, :purchase_id, presence: true
   validates :purchase_id, uniqueness: true
   validates :score, numericality: { only_integer: true, less_than_or_equal_to: 5 }
+
+  scope :find_scores_by, ->(column, start_date, end_date) { where("#{column} AND created_at >= ? AND created_at <= ? AND deleted='0'", start_date, end_date) }
 end
